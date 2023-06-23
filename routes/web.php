2784abefc5c9675d422741c9b713f4b76b55e5b7
daniel <daniel.stoyanovv@gmail.com>
2023-06-23 16:32:20 +0300

    Task: WOW-186 missed file

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::group(['prefix' => 'admin'], function () {
-    Route::resource('/users', UsersController::class);
-    Route::resource('/products', \App\Http\Controllers\Admin\ProductsController::class);
+Route::middleware('auth')->group(function () {
+    Route::group(['prefix' => 'admin'], function () {
+        Route::resource('/users', UsersController::class);
+        Route::resource('/products', \App\Http\Controllers\Admin\ProductsController::class);
+    });
 });
 
 Route::group(['prefix' => 'shop'], function () {
+    Route::get('/products', [\App\Http\Controllers\Shop\ProductsController::class, 'index'])->name('shop-products');
+    Route::group(['prefix' => 'cart'], function () {
+        Route::post('/add-product', [\App\Http\Controllers\Shop\CartController::class, 'addProduct'])->name('addProduct');
+    });
     // Checkout
     Route::group(['prefix' => 'checkout'], function () {
-        Route::get('create-transaction', [PayPalController::class, 'createTransaction'])->name('createTransaction');
         Route::post('process-transaction', [PayPalController::class, 'processTransaction'])->name('processTransaction');
         Route::get('success-transaction', [PayPalController::class, 'successTransaction'])->name('successTransaction');
         Route::get('cancel-transaction', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
